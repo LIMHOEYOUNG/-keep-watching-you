3a6025edaf03532ab36e392af7b0bd0276fcef55
gedhanz <119110156+gedhanz@users.noreply.github.com>
2024-03-17 20:19:51 +0900

    Add files via upload

---
+++ AI/drowsy_detection_cam.py
@@ -0,0 +1,35 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import uuid
+import os 
+import time
+import ultralytics
+from ultralytics import YOLO
+
+model = YOLO('best.pt')
+cap = cv2.VideoCapture(0)
+count = 0
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    
+    results_list = model(frame)
+    
+    for results in results_list:
+        font = cv2.FONT_HERSHEY_DUPLEX
+        annotated_image = results.plot()
+        cv2.imshow('YOLO', annotated_image)
+        confs = results.boxes.cls
+        for check in confs:
+            if check == 0:
+                count = count + 1
+        if count >= 20:
+            cv2.putText(frame, "drowsy dectection", (50,50), font, 1.0, (0, 0, 0), 1)
+            count = 0
+    
+    if cv2.waitKey(10) & 0xFF == ord("q"):
+        break
+        
+cap.release()
+cv2.destroyAllWindows()
