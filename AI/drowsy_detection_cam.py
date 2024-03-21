c81881599732b03438459fa5376559c7ebf89437
gedhanz <119110156+gedhanz@users.noreply.github.com>
2024-03-21 15:04:02 +0900

    Update drowsy_detection_cam.py

--- AI/drowsy_detection_cam.py
+++ AI/drowsy_detection_cam.py
@@ -20,19 +20,26 @@ while cap.isOpened():
     
     for results in results_list:
         confs = results.boxes.cls
+        confs = confs.type(torch.int32)
         for check in confs:
             if check == 0:
                 eyecount = eyecount + 1
+                print("eyecount1:" , eyecount)
             elif check == 3:
                 mouthcount = mouthcount + 1
+                print("mouthcount2:", mouthcount)
+            elif check == 1:
+                if(eyecount > 0):
+                    eyecount = eyecount - 2
+                print("eyecount3:" , eyecount)
+            elif check == 2:
+                if(mouthcount > 0):
+                    mouthcount = mouthcount - 1
+                print("mouthcount4:", mouthcount)
         if eyecount >= 20:
-            cv2.putText(frame, "drowsy dectection", (50,50), font, 1.0, (0, 0, 0), 1)
-            eyecount = 0
-            time.sleep(5)
+            cv2.putText(frame, "drowsy dectection eyecount: %d" %eyecount, (50,50), font, 1.0, (0, 0, 0), 1)
         if mouthcount >= 25:
-            cv2.putText(frame, "drowsy dectection", (50,50), font, 1.0, (0, 0, 0), 1)
-            mouthcount = 0
-            time.sleep(5)
+            cv2.putText(frame, "drowsy dectection mouthcount: %d" %mouthcount, (50,50), font, 1.0, (0, 0, 0), 1)
         annotated_image = results.plot()
         cv2.imshow('YOLO', annotated_image)
     
