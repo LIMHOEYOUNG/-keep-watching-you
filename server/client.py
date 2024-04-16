68bde0a8a3cf4e97710880385e37ba45f14af623
Donghyeok Lee <61753365+tnp22@users.noreply.github.com>
2024-04-16 20:31:20 +0900

    Delete server/client.py
    ggf

--- server/client.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:8000/login/"
-data = {"username": "test", "password": "passwd","jwt":
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjExNDExOTEsImVtYWlsIjoidGVzdCJ9._uYZdTNvoXtVfBivE9wB3aMeoYS4BWp8wXWCxxmV09Y"}
-headers = {"Authorization": "token"}
-response = requests.post(url, data=data)
-
-print(response.text)
