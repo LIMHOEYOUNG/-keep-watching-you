ac2bb74f931341b4831b537f774e2a8fe2b6a5ee
Hyeongjin Choi <119110156+gedhanz@users.noreply.github.com>
2024-05-27 23:11:49 +0900

    Update face_recognition.py

--- AI/face_recognition.py
+++ AI/face_recognition.py
@@ -1,6 +1,5 @@
 import os
 import os.path
-from PIL import Image, ImageDraw
 import face_recognition
 import cv2
 import pickle
@@ -51,31 +50,29 @@ def predict(X_img_path, knn_clf=None, model_path=None, distance_threshold=0.4):
 
     return [(pred, loc) if rec else ("Unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
 
-def show_prediction_labels_on_image(img_path, predictions):
-    pil_image = Image.open(img_path).convert("RGB")
-    draw = ImageDraw.Draw(pil_image)
-
-    for name, (top, right, bottom, left) in predictions:
-        draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
-        name = name.encode("UTF-8")
-        text_width, text_height = draw.textsize(name)
-        draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
-        draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
-
-    del draw
-    pil_image.show()
-
 if __name__ == "__main__":
+    unknown_count = 0
+    max_attempts = 3
 
-    captured_image_path = capture_image_on_face_detection()
-    if captured_image_path:
-        print(f"Looking for faces in {captured_image_path}")
-        predictions = predict(captured_image_path, model_path="trained_knn_model.clf")
-        for name, (top, right, bottom, left) in predictions:
-            if name == "Unknown":
-                print(f"- Found {name} at ({left}, {top})")
-                #서버에 사진과 비인가자 알림 전송
-            else:
-                subprocess.run(['python', 'drowsy_detection_cam.py'])
-        show_prediction_labels_on_image(captured_image_path, predictions)
+    while unknown_count < max_attempts:
+        captured_image_path = capture_image_on_face_detection()
+        if captured_image_path:
+            print(f"Looking for faces in {captured_image_path}")
+            predictions = predict(captured_image_path, model_path="trained_knn_model.clf")
+            for name, (top, right, bottom, left) in predictions:
+                if name == "Unknown":
+                    print(f"- Found {name} at ({left}, {top})")
+                    unknown_count += 1
+                    if unknown_count >= max_attempts:
+                        # 서버에 사진과 비인가자 알림 전송
+                        print("Unauthorized person detected multiple times. Sending alert to server.")
+                        #서버로 사진과 알림을 전송하는 코드
+                else:
+                    subprocess.run(['python', 'drowsy_detection_cam.py'])
+                    unknown_count = 0  # 인증된 사용자가 발견되면 시도 횟수 초기화
+                    break  # 인증된 사용자를 찾으면 더 이상의 검사를 중단
 
+    if unknown_count >= max_attempts:
+        # 서버에 사진과 비인가자 알림 전송
+        print("Unauthorized person detected multiple times. Sending alert to server.")
+        #서버로 사진과 알림을 전송하는 코드
